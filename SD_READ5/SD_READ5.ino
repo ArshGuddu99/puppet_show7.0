c2ed08027e306780ed2791633e2832f28feb209c
ArshGuddu99 <arshadguddu99@gmail.com>
2018-09-26 23:47:33 +0530

    Including cases for stopping specific or both the motors..partly successful as of 26th Sep

---
+++ SD_READ5/SD_READ5.ino
@@ -0,0 +1,93 @@
+#include<SD.h>
+#include<SPI.h>
+#define no_of_74hc594 1
+#define numOfRegisterPins no_of_74hc594 * 8
+const int ser=7;
+const int rclk=6;
+const int srclk=5;
+
+boolean registers[numOfRegisterPins];
+
+File myFile;
+
+int binarr[8];
+int binary;
+int file_read;
+int reading;
+int i,j;
+void clearRegisters(){
+  for(int i = 7; i >=  0; i--){
+     registers[i] = LOW;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+  if(!SD.begin(4)){
+    Serial.println("INIT FAILED");
+    delay(3000);
+    exit(0);
+  }
+  Serial.println("INIT Successful");
+  myFile=SD.open("data.txt",FILE_READ);
+  pinMode(ser,OUTPUT);
+  pinMode(rclk,OUTPUT);
+  pinMode(srclk,OUTPUT);
+}
+
+void arrtoint(){
+  file_read=0;
+  while(myFile.available())
+    {reading=myFile.read()-'0';
+      if(reading!='\n'&&reading>=0)
+        file_read=10*file_read+reading;
+       else
+        break; }
+        }
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+ if(myFile.available()){ 
+  arrtoint();
+  Serial.println(file_read);
+  delay(1500);
+for(j=0;j<500;j++){
+digitalWrite(rclk, LOW);
+  shiftOut(ser,srclk,MSBFIRST,file_read);
+  digitalWrite(rclk, HIGH);
+delay(1);
+  if(file_read/100==0)
+{digitalWrite(rclk, LOW);
+  shiftOut(ser,srclk,MSBFIRST,B00000000);
+  digitalWrite(rclk, HIGH);
+delay(1);}
+  if(file_read/100==11)
+  {digitalWrite(rclk, LOW);
+  shiftOut(ser,srclk,MSBFIRST,B00001100);
+  digitalWrite(rclk, HIGH);
+delay(1);}
+  if(file_read/100==1){
+    digitalWrite(rclk, LOW);
+  shiftOut(ser,srclk,MSBFIRST,B00000100);
+  digitalWrite(rclk, HIGH);
+  }
+  if(file_read/100==10){
+      digitalWrite(rclk, LOW);
+  shiftOut(ser,srclk,MSBFIRST,B00001000);
+  digitalWrite(rclk, HIGH);
+  }
+  }
+  }
+  else
+   { 
+     Serial.println("Nothing to read");
+     exit(0);}
+}
+
